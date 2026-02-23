fedc8782b3bce16d5ec6b1f8b26fc232e9a90b9d
amir_k <149391amir_k@users.no-reply.gitverse.ru>
2026-02-23 16:53:31 +0000

    update: main.py

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
-def find(numbers):
-    result = []
-    nums = list([-5, 1, 12, 17, 0, 13, 8, 3, 8])
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-    for i in range(len(nums)):
-        current = nums[i]
-        min_diff = float('inf')
-        correct = None
+app = FastAPI()
 
-        for j in range(len(nums)):
-            if i == j:
-                continue
 
-            diff = abs(current - nums[j])
+class AnimalCreateSchema(BaseModel):
+    type: str
+    name: str
 
-            if diff < min_diff:
-                min_diff = diff
-                correct = nums[j]
-            elif diff == min_diff and nums[j] < correct:
-                correct = nums[j]
 
-        result.append(correct)
+class AnimalSchema(BaseModel):
+    id: int
+    type: str
+    name: str
 
-    return result
 
-numbers = [-5, 1, 12, 17, 0, 13, 8, 3, 8]
-result = find(numbers)
-print(result)
+class AnimalUpdateSchema(BaseModel):
+    type: str | None = None  
+    name: str | None = None
+
+
+@app.post("/animals")
+def create_animal(body: AnimalCreateSchema) -> AnimalSchema:
+    return AnimalSchema(id=1, type=body.type, name=body.name)
+
+
+@app.get("/animals/{id}")
+def get_animal(id: int) -> AnimalSchema:
+    return AnimalSchema(id=1, type="cat", name="Мурзик")
+
+
+@app.get("/animals")
+def get_all_animals(
+    name: str | None = None, type: str | None = None
+) -> list[AnimalSchema]:
+    return [
+        AnimalSchema(id=1, type="cat", name="Мурзик"),
+        AnimalSchema(id=2, type="cat", name="Барсик"),
+        AnimalSchema(id=3, type="dog", name="Шарик")
+    ]
+
+
+@app.put("/animals/{id}")
+def update_animal(id: int, body: AnimalUpdateSchema) -> AnimalSchema:
+    return AnimalSchema(
+        id=id,
+        type=body.type if body.type is not None else "cat",  
+        name=body.name if body.name is not None else "Мурзик"
+    )
+
+
+@app.delete("/animals/{id}", status_code=204)
+def delete_animal(id: int) -> None:
+    return None  
+
+if __name__ == "__main__":
+    uvicorn.run(app)
